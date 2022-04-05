f40544e198cb37b283ca6ffa10cbe8ae70b3028c
O121009_D <zeraf29@gmail.com>
2022-04-05 09:33:01 +0900

    Function Types lecture

---
+++ src/lectures/FunctionTypes.kt
@@ -0,0 +1,26 @@
+package assignments
+
+/**
+ * isEmptyOrNull()
+ *Add and implement an extension function 'isEmptyOrNull()' on the type String?.
+ *It should return true, if the string is null or empty.
+ */
+
+fun main(args: Array<String>) {
+
+    //paramter: Int x, Int y. return is Int (x+y)
+    val sum = { x: Int, y: Int -> x + y }
+
+    //paramters: Int
+    //return: Boolean
+    //isEven variable's type: (Int) -> Boolean
+    //This is same-> val isEven: (Int) -> Boolean = { i: Int -> i % 2 == 0}
+    val isEven = { i: Int -> i % 2 == 0 }
+    //Lambda is the whole function containing the logic of how to compute the Boolean result from the given Int argument
+    //while the result is simply a Boolean value.
+
+    //You can call a variable a function type as a regular function, providing all the unnecessary arguments.
+
+
+
+}
