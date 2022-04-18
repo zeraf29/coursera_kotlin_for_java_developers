d488c8025ccd717925bdf1544e16baa14a9d06da
zeraf29 <zeraf29@gmail.com>
2022-04-19 07:35:55 +0900

    Member references

--- src/lectures/MemberReferences.kt
+++ src/lectures/MemberReferences.kt
@@ -1,6 +1,10 @@
 package lectures
 
 
+data class Person(
+    val name: String,
+    val age: Int
+)
 
 
 fun main(args: Array<String>) {
@@ -9,6 +13,25 @@ fun main(args: Array<String>) {
     //or return a member property, it can convert Lambda to member reference automatically when it's possible.
 
 
+    //you can store Lambda in a variable,
+    val isEven: (Int) -> Boolean = { i: Int -> i%2 ==0}
+    //however, you can't store a function in a variable.
+    fun isEven2(i: Int): Boolean = i%2 == 0
+    //val predicate = isEven2   //Compiler error
+
+
+    //Use function reference instead
+    //Function references allow you to store a reference to any defined function
+    // in a variable to be able to store it and qualitative it.
+    val predicate = ::isEven2
+    //Keep in mind that this syntax is just another way to call a function inside the Lambda,
+    // underlying implementation are the same
+    val predicate2 = { i: Int -> isEven2(i) }
+
+
+
+
 
 
 }
+
