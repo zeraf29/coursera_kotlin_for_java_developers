31ceccc8c9396df4dd3765fe32ff23d5372639be
zeraf29 <zeraf29@gmail.com>
2022-04-19 06:28:49 +0900

    Member references

---
+++ src/lectures/MemberReferences.kt
@@ -0,0 +1,14 @@
+package lectures
+
+
+
+
+fun main(args: Array<String>) {
+
+    //Like Java, Kotlin has member references, which can replace simple Lambdas that only call a member function
+    //or return a member property, it can convert Lambda to member reference automatically when it's possible.
+
+
+
+
+}
