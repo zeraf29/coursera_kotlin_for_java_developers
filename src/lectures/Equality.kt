70dfca4ae11d76e1e29924a2caacb4dcccf78e5e
zeraf29 <zeraf29@gmail.com>
2022-09-23 11:13:26 +0900

    Playground - Equality

---
+++ src/lectures/Equality.kt
@@ -0,0 +1,25 @@
+package lectures
+
+import common.function.eq
+
+data class Value(val s: String)
+
+fun equals1(v1: Value?, v2: Value?): Boolean {
+    return v1 == v2
+}
+
+fun equals2(v1: Value?, v2: Value?): Boolean = v1?.equals(v2) ?: (v2 === null)
+//equals can't be used for null
+//if v1 is null, check v2 is null or not. if v2 is also null, return is true.
+
+fun main(args: Array<String>) {
+    equals1(Value("abc"), Value("abc")) eq true
+    equals1(Value("abc"), null) eq false
+    equals1(null, Value("abc")) eq false
+    equals1(null, null) eq true
+
+    equals2(Value("abc"), Value("abc")) eq true
+    equals2(Value("abc"), null) eq false
+    equals2(null, Value("abc")) eq false
+    equals2(null, null) eq true
+}
