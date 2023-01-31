0f22f609fd384a4e93449e3f5240ff9471be3538
zeraf29 <zeraf29@gmail.com>
2023-02-01 07:25:12 +0900

    Week5 assignment. Game2048Helper.kt : 3 test were not passed

---
+++ src/assignments/Games/Task/src/games/game2048/Game2048Helper.kt
@@ -0,0 +1,37 @@
+package games.game2048
+
+/*
+ * This function moves all the non-null elements to the beginning of the list
+ * (by removing nulls) and merges equal elements.
+ * The parameter 'merge' specifies the way how to merge equal elements:
+ * it returns a new element that should be present in the resulting list
+ * instead of two merged elements.
+ *
+ * If the function 'merge("a")' returns "aa",
+ * then the function 'moveAndMergeEqual' transforms the input in the following way:
+ *   a, a, b -> aa, b
+ *   a, null -> a
+ *   b, null, a, a -> b, aa
+ *   a, a, null, a -> aa, a
+ *   a, null, a, a -> aa, a
+ *
+ * You can find more examples in 'TestGame2048Helper'.
+*/
+fun <T : Any> List<T?>.moveAndMergeEqual(merge: (T) -> T): List<T> {
+        var buffer = ""
+        val rList = mutableListOf<T>()
+        this.filterNotNull().forEach {
+                if(buffer.contains(it.toString())) buffer += it.toString()
+                else{
+                        buffer = if(buffer.isBlank()){
+                                it.toString()
+                        }else{
+                                rList.add(buffer as T)
+                                it.toString()
+                        }
+                }
+        }
+        if(buffer.isNotBlank()) rList.add(buffer as T)
+        return rList
+}
+
